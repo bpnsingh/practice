c684bca3bb8eb1e667f4c53fb86cbb4374083f5b
bipinsengar <bsengar@dialpad.com>
2021-09-28 23:49:26 +0530

    check prime number bool without math

--- pythonprograms/scaler/check_prime.py
+++ pythonprograms/scaler/check_prime.py
@@ -2,17 +2,18 @@ import math
 
 def check_prime_bool(N):
     if N<2:
+        #To handle negative and 1
         return False
     i=2
     loop_limit= int(math.sqrt(N))
-    while (i <= loop_limit):
+    while (i*i <= N):
         if N%i == 0:
             return False
         i+=1
     return True
 
 if __name__ == "__main__":
-    #print (check_prime_bool(1))
-    #print(check_prime_bool(2))
-    #print(check_prime_bool(5))
+    print (check_prime_bool(1))
+    print(check_prime_bool(2))
+    print(check_prime_bool(5))
     print(check_prime_bool(50))
