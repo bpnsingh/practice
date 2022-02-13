86ab32a2a520b198f840160ed8dea52b386102ee
bipinsengar <bsengar@dialpad.com>
2022-02-13 16:05:45 +0530

    merge sorted subarray

--- scaler/sorting/insertion_sort.py
+++ scaler/sorting/insertion_sort.py
@@ -5,12 +5,13 @@ class Solution:
         A = A^B
         B = A^B
         A = A^B
+        return None
     def indrtion_sort(self, A):
         N = len(A)
         for i in range(1,N):
             j = i-1
             while j >= 0 and A[j] > A[j+1] :
-                self.swap(A[j],A[j+1])
+                A[j],A[j+1] = A[j+1] , A[j]
                 j -= 1
         return A
 
